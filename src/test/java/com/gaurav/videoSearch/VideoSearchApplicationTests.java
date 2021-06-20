382dd21191b739a69249d5eb1254f1df7c8a6bf7
jgaurav <Gaurav2.Jaiswal@ril.com>
2021-06-20 17:06:38 +0530

    Added resource and test folder

---
+++ src/test/java/com/gaurav/videoSearch/VideoSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.videoSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
