bb6da465479dbd81785240dc0ea79960f6815f75
jgaurav <Gaurav2.Jaiswal@ril.com>
2021-06-20 15:44:33 +0530

    Added constant file

---
+++ src/main/java/com/gaurav/videoSearch/constant/Constant.java
@@ -0,0 +1,12 @@
+package com.gaurav.videoSearch.constant;
+
+public class Constant {
+    public static final String PAGE_SIZE = "pageSize";
+    public static final String PAGE_NUMBER = "pageNumber";
+    public static final String BASE_URL = "https://youtube.googleapis.com/youtube/v3/search";
+    public static final String PART = "snippet";
+    public static final String API_KEY = "AIzaSyByptaIKlfRY9ul0TUVWXoeZH8KhtXKNF4";
+    public static final int MAX_RESULT = 50;
+    public static final String QUERY = "cricket";
+
+}
